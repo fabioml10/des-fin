a5ff76e67fb3de9ba08e3db4ea62541da456b74e
Fabio Muller <fabioml10@gmail.com>
2022-07-07 12:41:22 -0300

    Add missing tests of CreateAccount service

--- spec/services/create_account_spec.rb
+++ spec/services/create_account_spec.rb
@@ -2,39 +2,38 @@ RSpec.describe CreateAccount do
   describe "#call" do
     subject(:call) { described_class.call(payload) }
 
+    let(:payload) do
+      {
+        name: name,
+        users: [
+          {
+            first_name: Faker::Name.first_name,
+            last_name: Faker::Name.last_name,
+            email: Faker::Internet.email,
+            phone: "(11) 97111-0101",
+          },
+        ],
+      }
+    end
+
     context "when account is created" do
-      let(:payload) do
-        {
-          name: Faker::Company.name,
-          users: [
-            {
-              first_name: Faker::Name.first_name,
-              last_name: Faker::Name.last_name,
-              email: Faker::Internet.email,
-              phone: "(11) 97111-0101",
-            },
-          ],
-        }
-      end
       let(:expected_result) { ApplicationService::Result.new(true, Account.last, nil) }
 
-      it { is_expected.to eql(expected_result) }
+      context "with name Fintera" do
+        let(:name) { "Fintera" }
+
+        it { is_expected.to eql(expected_result) }
+      end
+
+      context "without name Fintera" do
+        let(:name) { Faker::Company.name }
+
+        it { is_expected.to eql(expected_result) }
+      end
     end
 
     context "when account is not created" do
-      let(:payload) do
-        {
-          name: "",
-          users: [
-            {
-              first_name: Faker::Name.first_name,
-              last_name: Faker::Name.last_name,
-              email: Faker::Internet.email,
-              phone: "(11) 97111-0101",
-            },
-          ],
-        }
-      end
+      let(:name) { "" }
       let(:expected_result) { ApplicationService::Result.new(false, nil, "Name can't be blank") }
 
       it { is_expected.to eql(expected_result) }
