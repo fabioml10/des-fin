08f8aa0dae51cfe7a29b1104aede7a33954fd305
Fabio Muller <fabioml10@gmail.com>
2022-07-07 13:04:34 -0300

    Remove user and account association

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,5 @@
 RSpec.describe User do
   describe "associations" do
-    it { is_expected.to belong_to :account }
     it { is_expected.to have_many :entities_users }
     it { is_expected.to have_many(:entities).through(:entities_users) }
   end
